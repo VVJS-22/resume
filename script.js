926b27cb6d91fa55c257d47f789ae66e8629f6ee
VVJS-22 <s.jayeshchennai@gmail.com>
2021-06-16 11:14:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// const resume = document.getElementById("wrapper");
+// const button = document.getElementById('button')
+
+// function generatePdf () {
+//   html2pdf(resume, opt);
+// }
+
+// const opt = {
+//   margin: 0,
+//   filename: "myResume.pdf",
+//   image: { type: 'jpeg', quality: 0.98},
+//   html2canvas: { scale: 4},
+//   jsPDF: { format: 'a4', orientation:  'portrait'}
+// }
+
+// button.addEventListener('click', generatePdf);
